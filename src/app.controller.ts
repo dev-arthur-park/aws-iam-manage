b5fd4bacfe294ef8a1920f76b6b70adcd6064135
arthur-park <>
2023-03-01 14:19:29 +0900

    download 구현 path 변경 - aws/list - aws/download

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, Query } from '@nestjs/common';
+import { Controller, Get, Res, Query, Header } from '@nestjs/common';
 import { AppService, QueryType } from './app.service';
 
 @Controller()
@@ -10,11 +10,55 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("/AccessKeylist")
-  async listAWSIAM(@Query() q: QueryType): Promise<any> {
+  @Get("/aws/list")
+  async listAWSiAM(@Query() q: QueryType , @Res() res): Promise<any> {
     if (q?.period && !Number(q?.period)) {
-      return "period 값은 숫자만 입력해야 합니다."
+      return res.send({
+        status: 400, 
+        MSG: "period 값은 숫자만 입력해야 합니다."
+      })
     }
-    return await this.appService.getAWSOldAccesskey(q)
+
+    const reuslt = await this.appService.list(q)
+
+    if (reuslt && reuslt == 500) {
+      return res.send(
+        {
+          status: 500,
+          MSG: "server error"
+        }
+      )
+    }
+
+    return res.send(
+      {
+        status : 200,
+        reuslt
+      }
+    )
+  }
+
+  @Get("/aws/download")
+  @Header('Content-type', 'application/txt')
+  async listAWSiAMtoFile(@Query() q: QueryType, @Res() res): Promise<{} | FileSystem> {
+    if (q?.period && !Number(q?.period)) {
+      return res.send({
+        status: 400, 
+        MSG: "period 값은 숫자만 입력해야 합니다."
+      })
+    }
+    
+    const reuslt = await this.appService.download(q)
+
+    if (reuslt && reuslt == 500) {
+      return res.send(
+        {
+          status: 500,
+          MSG: "server error"
+        }
+      )
+    }
+
+    return await res.send(reuslt)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 import { ListAccessKeysCommand } from "@aws-sdk/client-iam"
 import { IAMClient } from "@aws-sdk/client-iam"
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
 
 @Injectable()
 export class AppService {
@@ -8,33 +10,61 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async getAWSOldAccesskey(q?: QueryType) {
+  async list(q?: QueryType) {
+    try {
+      return await this.getAwsAccesKeyList(q)
+    } catch (err) {
+      console.log("app.service.list", err)
+      return 500
+    }
+  }
+  
+  async download(q: QueryType) {
+    try {
+      const makefilename = await this.makefile(q)
+      return readFileSync(makefilename)
+    } catch (err) {
+      console.log("app.service.list", err)
+      return 500
+    }
+  }
+
+  private async makefile(q: QueryType) {
+    const savePath = join(__dirname, "../resources");
+    const saveFileName = join(savePath, "result.txt");
+    const saveData = await this.getAwsAccesKeyList(q)
+    
+    if (!existsSync(savePath)) {
+      mkdirSync(savePath, { recursive:true });
+    }
+    
+    writeFileSync(saveFileName, JSON.stringify(saveData));
+    
+    return saveFileName
+  }
+
+  private async getAwsAccesKeyList(q?: QueryType){
     const iamClient = new IAMClient({})
     const params = { MaxItems: 100 };
 
     const hours = q?.period || 24 * 30
 
-    try {
-      const CurrentDate = new Date()
-      const TartgetDate = new Date(CurrentDate.setHours(CurrentDate.getHours() - hours))
-      
-      const data = await iamClient.send(new ListAccessKeysCommand(params))
-      const keysData = data.AccessKeyMetadata || [];
-      
-      const result = keysData
-      .filter((key) => key.CreateDate > TartgetDate)
-      .map((key) => {
-        console.log("User " + key.UserName + " created", key.CreateDate);
-        return {
-          UserName: key.UserName, 
-          AccessKeyID: key.AccessKeyId, 
-          CreateDate: key.CreateDate}
-      });
-      console.log("reuslt", result)
-      return result;
-    } catch (err) {
-      console.log("Error", err);
-    }
+    const CurrentDate = new Date()
+    const TartgetDate = new Date(CurrentDate.setHours(CurrentDate.getHours() - hours))
+    
+    const data = await iamClient.send(new ListAccessKeysCommand(params))
+    const keysData = data.AccessKeyMetadata || [];
+    
+    const result = keysData
+    .filter((key) => key.CreateDate < TartgetDate)
+    .map((key) => {
+      return {
+        UserName: key.UserName, 
+        AccessKeyID: key.AccessKeyId, 
+        CreateDate: key.CreateDate
+      }
+    })
+    return result;
   }
 }
 
